06be4a1955815ebf0d4a9b2a607b89450f3d3cbe
Miguel <miguel.sol.1997@outlook.com>
2019-10-20 16:24:27 -0500

    Deberia funcionar

--- app/src/main/java/com/miguel/arduinocar/ConnectBT.java
+++ app/src/main/java/com/miguel/arduinocar/ConnectBT.java
@@ -8,15 +8,16 @@ import android.os.AsyncTask;
 import java.io.IOException;
 import java.util.UUID;
 
-public class ConnectBT extends AsyncTask<String, Void, BluetoothSocket> {
+public class ConnectBT extends AsyncTask<String, Void, String> {
     private OnConnectListener listener;
     private OnErrorListener errorListener;
 
     public interface OnConnectListener {
         void success(BluetoothSocket btSocket);
     }
+
     public interface OnErrorListener {
-        void error(Exception ex);
+        void error(String ex);
     }
 
     private boolean connectSuccess = true; //if it's here, it's almost connected
@@ -26,12 +27,15 @@ public class ConnectBT extends AsyncTask<String, Void, BluetoothSocket> {
         this.listener = listener;
         this.errorListener = errorListener;
     }
+
     public ConnectBT(OnConnectListener listener) {
         this(listener, null);
     }
 
+    private BluetoothSocket btSocket;
+
     @Override
-    protected BluetoothSocket doInBackground(String... strings) {
+    protected String doInBackground(String... strings) {
         try {
             String address = strings[0];
             BluetoothAdapter myBluetooth = BluetoothAdapter.getDefaultAdapter();//get the mobile bluetooth device
@@ -39,19 +43,22 @@ public class ConnectBT extends AsyncTask<String, Void, BluetoothSocket> {
             BluetoothSocket btSocket = dispositivo.createInsecureRfcommSocketToServiceRecord(UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"));//create a RFCOMM (SPP) connection
             BluetoothAdapter.getDefaultAdapter().cancelDiscovery();
             btSocket.connect();//start connection
-            return btSocket;
-        } catch (IOException e) {
-            connectSuccess = false;//if the try failed, you can check the exception here
-            if (errorListener != null) errorListener.error(e);
+            this.btSocket = btSocket;
             return null;
+        } catch (Exception e) {
+            connectSuccess = false;//if the try failed, you can check the exception here
+            return e.getMessage();
         }
     }
 
     @Override
-    protected void onPostExecute(BluetoothSocket bluetoothSocket) {
-        super.onPostExecute(bluetoothSocket);
-        if (listener != null && bluetoothSocket != null) {
-            listener.success(bluetoothSocket);
+    protected void onPostExecute(String error) {
+        super.onPostExecute(error);
+        if (listener != null && error == null) {
+            listener.success(btSocket);
+        } else if (listener != null) {
+            errorListener.error(error);
+
         }
 
     }
--- app/src/main/java/com/miguel/arduinocar/MainActivity.java
+++ app/src/main/java/com/miguel/arduinocar/MainActivity.java
@@ -7,34 +7,65 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.jmedeisis.bugstick.Joystick;
+import com.jmedeisis.bugstick.JoystickListener;
+
 import java.io.IOException;
-import java.io.OutputStream;
-import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
 
     private BluetoothSocket btSocket;
+    private Button btnConectar;
+    private static final String TAG = MainActivity.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Button btn = findViewById(R.id.button);
-
-
-        btn.setOnClickListener(new View.OnClickListener() {
+        final EditText txtMac = findViewById(R.id.txtMac);
+        btnConectar = findViewById(R.id.btnConectar);
+        btnConectar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                test();
+                String mac = txtMac.getText().toString();
+                connectBt(mac);
             }
         });
+
+
+        Joystick joy = findViewById(R.id.joystick);
+
+        joy.setJoystickListener(new JoystickListener() {
+            @Override
+            public void onDown() {
+
+            }
+
+            @Override
+            public void onDrag(float degrees, float offset) {
+                char[] toSend = {'6', '3', '2', '1', '4', '7', '8', '9'};
+                int val = ((int) degrees + 360) % 360;
+                val = (val + 22) % 360;
+                val = val / 45;
+                Log.d(TAG, "Value to send " + toSend[val]);
+                sendBt(toSend[val] + "");
+            }
+
+            @Override
+            public void onUp() {
+                Log.d(TAG, "Stopping car");
+                sendBt("w");
+            }
+        });
+
     }
 
-    private void test() {
-
+    private void connectBt(String mac) {
+        btnConectar.setEnabled(false);
         new ConnectBT(new ConnectBT.OnConnectListener() {
             @Override
             public void success(BluetoothSocket btSocket) {
@@ -46,15 +77,25 @@ public class MainActivity extends AppCompatActivity {
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-
-
             }
         }, new ConnectBT.OnErrorListener() {
             @Override
-            public void error(Exception ex) {
-                Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_LONG).show();
+            public void error(String ex) {
+                Toast.makeText(MainActivity.this, "Error " + ex, Toast.LENGTH_LONG).show();
+                btnConectar.setEnabled(true);
             }
-        }).execute("20:18:04:15:44:74");
+        }).execute(mac);
+    }
+
+    private void sendBt(String content) {
+        if (btSocket == null) return;
+        try {
+            Log.d("Sending", "Sending: " + content);
+            btSocket.getOutputStream().write(content.getBytes());
+        } catch (Exception e) {
+            Log.e("Sending", "Failed to send : " + content + "----> " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 
     @Override
